1319b5b239ee37fcadd28c10ca25840479d1b7c7
mlacerda01 <marttalacerda@gmail.com>
2020-07-04 18:16:50 -0300

    Add files via upload
    API que lista os repositórios do .NET Foundation

---
+++ WebAPIClient/Controllers/GitHubController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebAPIClient.Models;
+
+namespace WebAPIClient.Controllers
+{
+    [Route("api/git")]
+    public class GitHubController : Controller
+    {
+        private static readonly HttpClient client = new HttpClient();
+       
+        [HttpGet]
+        [Route("repositories")]
+        public async Task<IEnumerable<Repository>> ProcessRepositories()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));            
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var streamTask = await client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+            var repositories = await JsonSerializer.DeserializeAsync<IEnumerable<Repository>>(streamTask);
+
+            return repositories;
+        }
+    }
+}
---
+++ WebAPIClient/Models/Repository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace WebAPIClient.Models
+{
+    public class Repository
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("html_url")]
+        public Uri GitHubHomeUrl { get; set; }
+
+        [JsonPropertyName("homepage")]
+        public Uri Homepage { get; set; }
+
+        [JsonPropertyName("watchers")]
+        public int Watchers { get; set; }
+
+        [JsonPropertyName("pushed_at")]
+        public DateTime LastPushUtc { get; set; }
+        public DateTime LastPush => LastPushUtc.ToLocalTime();
+    }
+}
